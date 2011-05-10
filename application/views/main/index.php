718b8350fa1fc59089f581905c185065573a6748
Andrew Vayanis <avayanis@slevin.home>
2011-05-10 02:12:07 -0400

    Updated sample controller

--- application/views/main/index.php
+++ application/views/main/index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html> 
 <html lang="en"> 
     <head>
-        <title>ThinMVC Installation Successful!</title>
+        <title>Mighty MVC Installation Successful!</title>
     </head>
     <body>
-        <h1>Welcome to ThinMVC</h1>
-        <p>If you are seeing this page, then your ThinMVC installation is successful.</p>
-        <p>Page rendered in: <?=number_format((microtime() - MM::$start_time), 3);?> seconds.</p>
+        <h1>Welcome to  Mighty MVC</h1>
+        <p>If you are seeing this page, then your Mighty MVC installation is successful.</p>
+        <p>Page rendered in: <?=number_format((microtime(true) - MM::$start_time), 3);?> seconds.</p>
     </body>
 </html>
