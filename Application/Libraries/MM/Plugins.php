e6a8a82170feac67e79ede99a9ed2c6257b89664
Andrew Vayanis <avayanis@slevin.riotgames.com>
2011-09-16 12:36:17 -0700

    Updated MM_Layout to properly reference instance.

--- Application/Libraries/MM/Plugins.php
+++ Application/Libraries/MM/Plugins.php
@@ -204,8 +204,8 @@ class MM_Layout
         // Configure layout
         $instance->setLayout($config['default']);
         
-        MM::register('post-dispatch', function($layout) use ($instance) {
-            $layout->render();
+        MM::register('post-dispatch', function() use ($instance) {
+            $instance->render();
         }, (PHP_INT_MAX * -1)-1);
     }
 
